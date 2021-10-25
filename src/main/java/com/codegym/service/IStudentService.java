c99771439406d5e740861c6192713536b802de6e
manhdung98na <md0949470893@gmail.com@github.com>
2021-10-25 21:28:44 +0700

    Xem danh sách học sinh và xoá học sinh - 25/10

--- src/main/java/com/codegym/service/IStudentService.java
+++ src/main/java/com/codegym/service/IStudentService.java
@@ -1,5 +1,13 @@
 package com.codegym.service;
 
-public interface IStudentService {
+import com.codegym.entity.about_student.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+public interface IStudentService{
+    Page<Student> findByClassroom(int classroomId, Pageable pageable);
+
+    Student getById(int id);
+
+    Student deleteById(int studentId);
 }
