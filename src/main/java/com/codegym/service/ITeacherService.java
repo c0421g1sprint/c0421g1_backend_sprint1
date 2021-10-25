8de9c896f1a68dd0c815adc67500dbfefa8f517d
Ngô Nhật Minh <minhngo171997@gmail.com>
2021-10-26 03:07:36 +0700

    Minh update 25/10 infor teacher

--- src/main/java/com/codegym/service/ITeacherService.java
+++ src/main/java/com/codegym/service/ITeacherService.java
@@ -1,4 +1,13 @@
 package com.codegym.service;
 
+import com.codegym.dto.TeacherDto;
+import com.codegym.entity.about_teacher.Teacher;
+
 public interface ITeacherService {
+
+
+    //    MinhNN 24/10
+    Teacher getById(int id);
+
+    void update(TeacherDto teacherDto);
 }
