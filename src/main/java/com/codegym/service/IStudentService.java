3a53418af97cf013fc9a76d7a3bef419baec391d
unknown <ndanh9698@gmail.com>
2021-10-26 01:13:17 +0700

    DanhNT update 12:00PM 25-10-21

--- src/main/java/com/codegym/service/IStudentService.java
+++ src/main/java/com/codegym/service/IStudentService.java
@@ -1,5 +1,23 @@
 package com.codegym.service;
 
-public interface IStudentService {
+import com.codegym.entity.about_student.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+public interface IStudentService{
+    //Trùng với chị Hà nhưng khác kiểu trả về
+    //Danh coding 2:00PM - 23-10-2021
+    void updateClassForStudent(Integer classId, Integer studentId);
+
+    //creator: HaNTT, date: 23/10/2021
+    Page<Student> findByClassroom(int classroomId, Pageable pageable);
+
+    //creator: HaNTT, date: 23/10/2021
+    Page<Student> findWhereClassroomIdNull(Pageable pageable);
+
+    //creator: HaNTT, date: 23/10/2021
+    Student findStudentById(Integer id);
+
+    //creator: HaNTT, date: 23/10/2021
+//    Integer setClassroomForNewStudent(Integer classRoomId, Integer studentId);
 }
