c67c41908393baaf61812a90fa19549e7901fcea
Ngô Nhật Minh <minhngo171997@gmail.com>
2021-10-26 03:06:36 +0700

    Minh update 25/10 infor teacher

--- src/main/java/com/codegym/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/codegym/service/impl/TeacherServiceImpl.java
@@ -1,5 +1,7 @@
 package com.codegym.service.impl;
 
+import com.codegym.dto.TeacherDto;
+import com.codegym.entity.about_teacher.Teacher;
 import com.codegym.repository.ITeacherRepository;
 import com.codegym.service.ITeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class TeacherServiceImpl implements ITeacherService {
     @Autowired
-    private ITeacherRepository teacherRepository;
+    private ITeacherRepository iTeacherRepository;
+    //MinhNN 24/10 find teacher by id account
+    @Override
+    public Teacher getById(int id) {
+        return iTeacherRepository.getById(id);
+    }
 
+
+    //MinhNN 24/10 update infor teacher
+    @Override
+    public void update(TeacherDto teacherDto) {
+        iTeacherRepository.editPersonInfor(teacherDto.getTeacherPhone(), teacherDto.getTeacherAddress(), teacherDto.getTeacherEmail(), teacherDto.getAccount().getAccountId());
+    }
 }
