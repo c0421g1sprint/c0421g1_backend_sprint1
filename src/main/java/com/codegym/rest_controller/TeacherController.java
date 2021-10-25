5134ed83e8e0095073d0dc4131271c51999468d0
Ngô Nhật Minh <minhngo171997@gmail.com>
2021-10-26 03:05:34 +0700

    Minh update 25/10 infor teacher

--- src/main/java/com/codegym/rest_controller/TeacherController.java
+++ src/main/java/com/codegym/rest_controller/TeacherController.java
@@ -1,11 +1,35 @@
 package com.codegym.rest_controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codegym.dto.TeacherDto;
+import com.codegym.entity.about_teacher.Teacher;
+import com.codegym.service.ITeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/teachers")
 public class TeacherController {
+    @Autowired
+    private ITeacherService iTeacherService;
+
+    //    MinhNN 24/10 find teacher by id account
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable("id") int id) {
+        Teacher teacher = iTeacherService.getById(id);
+        if (teacher != null) {
+            return new ResponseEntity<>(teacher, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //    MinhNN 24/10 update infor teacher
+    @PatchMapping("/update")
+    public ResponseEntity<?> updateInforTeacher(@RequestBody TeacherDto teacherDto) {
+        iTeacherService.update(teacherDto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
