7285c0e7286dbb677d9236bf78181d4397d2dac4
vantjn01 <levandiep1991@gmail.com>
2021-10-26 08:36:26 +0700

    diep push search teacher 26/10

--- src/main/java/com/codegym/service/ITeacherService.java
+++ src/main/java/com/codegym/service/ITeacherService.java
@@ -1,4 +1,13 @@
 package com.codegym.service;
 
-public interface ITeacherService {
+import com.codegym.entity.about_teacher.Teacher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ITeacherService{
+    // Diep search teacher 25/10
+//    Page<Teacher> searchTeacher(Pageable pageable, Integer teacherId, String teacherName,
+//                                Byte teacherGender, Date teacherDateOfBirth, String teacherPhone,
+//                                String teacherAddress);
+    Page<Teacher> searchTeacher(Pageable pageable, String search);
 }
--- src/main/java/com/codegym/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/codegym/service/impl/TeacherServiceImpl.java
@@ -1,8 +1,11 @@
 package com.codegym.service.impl;
 
+import com.codegym.entity.about_teacher.Teacher;
 import com.codegym.repository.ITeacherRepository;
 import com.codegym.service.ITeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,4 +13,9 @@ public class TeacherServiceImpl implements ITeacherService {
     @Autowired
     private ITeacherRepository teacherRepository;
 
+
+    @Override
+    public Page<Teacher> searchTeacher(Pageable pageable, String search) {
+        return teacherRepository.searchTeacher(pageable, "%" + search + "%");
+    }
 }
