9886c679856080149619cee2e65a67003fb695b0
c0421g1sprint <83199138+anhquan110297@users.noreply.github.com>
2021-10-27 08:45:12 +0700

    update Back-end 27/10/2021

--- src/main/java/com/codegym/rest_controller/ScheduleController.java
+++ src/main/java/com/codegym/rest_controller/ScheduleController.java
@@ -4,9 +4,11 @@ import com.codegym.DTO.ScheduleDetailDto;
 import com.codegym.entity.about_classroom.Classroom;
 import com.codegym.entity.about_classroom.Grade;
 import com.codegym.entity.about_schedule.ScheduleDetail;
+import com.codegym.entity.about_schedule.Subject;
 import com.codegym.service.IClassroomService;
 import com.codegym.service.IGradeService;
 import com.codegym.service.IScheduleService;
+import com.codegym.service.ISubjectService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -32,12 +34,15 @@ public class ScheduleController {
     @Autowired
     private IScheduleService scheduleService;
 
+    @Autowired
+    private ISubjectService subjectService;
+
     // // QuanTA 22/10 10h:46 pm api tra ve 1 list  grade
     @GetMapping(value = "/grades")
     public ResponseEntity<List<Grade>> showListGrade() {
         List<Grade> gradeList = this.gradeService.findAllGrade();
         if (gradeList == null) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
         }
         return new ResponseEntity<>(gradeList, HttpStatus.ACCEPTED);
     }
@@ -47,7 +52,7 @@ public class ScheduleController {
     public ResponseEntity<List<Classroom>> showListClassroomExist() {
         List<Classroom> classroomList = this.classroomService.findAllClassroomExist();
         if (classroomList == null) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
         }
         return new ResponseEntity<>(classroomList, HttpStatus.ACCEPTED);
     }
@@ -57,7 +62,7 @@ public class ScheduleController {
     public ResponseEntity<List<ScheduleDetail>> scheduleDetailClassroom(@PathVariable(required = false) Integer id) {
         List<ScheduleDetail> scheduleDetailListOfClassroom = this.scheduleService.findScheduleDetailByClassroomId(id);
         if (scheduleDetailListOfClassroom.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
         }
         return new ResponseEntity<>(scheduleDetailListOfClassroom, HttpStatus.ACCEPTED);
     }
@@ -74,5 +79,14 @@ public class ScheduleController {
         this.scheduleService.updateSchedule(scheduleDetail.getSubject().getSubjectId(),
                 scheduleDetail.getScheduleDetailId());
         return new ResponseEntity<>(scheduleDetail, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping(value = "/subject")
+    public ResponseEntity<List<Subject>> findAllSubject(){
+        List<Subject> subjectList = this.subjectService.findAllSubject();
+        if (subjectList == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+        return new ResponseEntity<>(subjectList, HttpStatus.ACCEPTED);
     }
 }
